b6368a916b2be82b23d08ddba423da8ed967b75f
psychome004 <anil.stephen66@gmail.com>
2021-03-23 15:28:50 +0530

    Added shift-post sow

---
+++ so-widgets/shift-post/shift-post.php
@@ -0,0 +1,76 @@
+<?php
+/*
+	Widget Name: Shift Post
+	Description: Shift Post
+	Author: Stephen Anil, Sputznik
+	Author URI:	http://sputznik.com
+	Widget URI:
+	Video URI:
+*/
+class Shift_Post_Card extends SiteOrigin_Widget {
+
+	function __construct() {
+		//Here you can do any preparation required before calling the parent constructor, such as including additional files or initializing variables.
+		//Call the parent constructor with the required arguments.
+		parent::__construct(
+			// The unique id for your widget.
+			'shift-post',
+			// The name of the widget for display purposes.
+			__('Shift Post', 'siteorigin-widgets'),
+			// The $widget_options array, which is passed through to WP_Widget.
+			// It has a couple of extras like the optional help URL, which should link to your sites help or support page.
+			array(
+				'description' => __('Shift Post', 'siteorigin-widgets'),
+				'help'        => '',
+			),
+			//The $control_options array, which is passed through to WP_Widget
+			array(),
+			//The $form_options array, which describes the form fields used to configure SiteOrigin widgets. We'll explain these in more detail later.
+			array(
+				'image' => array(
+					'type' 		=> 'media',
+					'label' 	=> __( 'Choose Image', 'siteorigin-widgets' ),
+					'choose' 	=> __( 'Choose image', 'siteorigin-widgets' ),
+					'update' 	=> __( 'Set image', 'siteorigin-widgets' ),
+					'library' 	=> 'image',
+					'fallback' 	=> false
+				),
+				'title_left' => array(
+					'type' 	=> 'text',
+					'label' => __( 'Tile Left', 'siteorigin-widgets'),
+				),
+				'tl_color' => array(
+					'type' 		=> 'color',
+					'label' 	=> __( 'Tile Left Color', 'siteorigin-widgets' ),
+					'default' 	=> '#000000'
+				),
+				'title_right' => array(
+					'type' 	=> 'text',
+					'label' => __( 'Title Right', 'siteorigin-widgets'),
+				),
+				'tr_color' => array(
+					'type' 		=> 'color',
+					'label' 	=> __( 'Tile Right Color', 'siteorigin-widgets' ),
+					'default' 	=> '#000000'
+				),
+				'content' => array(
+					'type' 	=> 'textarea',
+					'label' => __( 'Content', 'siteorigin-widgets'),
+				),
+			),
+			//The $base_folder path string.
+			get_template_directory()."/so-widgets/shift-post"
+		);
+	}
+
+	function get_template_name($instance) {
+		return 'template';
+	}
+	function get_template_dir($instance) {
+		return 'templates';
+	}
+    function get_style_name($instance) {
+        return '';
+    }
+}
+siteorigin_widget_register('shift-post', __FILE__, 'Shift_Post_Card');
---
+++ so-widgets/shift-post/templates/template.php
@@ -0,0 +1,23 @@
+<!-- SHIFT POST -->
+<div class="shift-post-wrapper">
+	<?php $image = wp_get_attachment_url( $instance['image'] );?>
+	<div class="post-item">
+		<div class="post-title">
+			<div class="title-left" style="color: <?php _e( $instance['tl_color'] );?>;">
+				<?php _e( $instance['title_left'] );?>
+			</div>
+			<div class="title-right" style="color: <?php _e( $instance['tr_color'] );?>;">
+				<?php _e( $instance['title_right'] );?>
+			</div>
+		</div>
+		<div class="post-bg-wrapper">
+			<div class="post-bg">
+				<img src="<?php _e( $image );?>" alt="Background image">
+			</div>
+			<div class="overlay"></div>
+		</div>
+		<div class="post-content-wrapper">
+			<div class="post-content"><?php _e( $instance['content'] );?></div>
+		</div>
+	</div>
+</div><!-- SHIFT POST -->
