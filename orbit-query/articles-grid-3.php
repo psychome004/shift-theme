6b6e76fbf5677e257e036a868e3620aaf956f680
psychome004 <anil.stephen66@gmail.com>
2021-03-30 12:18:34 +0530

    Added orbit-grid-3 template

---
+++ orbit-query/articles-grid-3.php
@@ -0,0 +1,7 @@
+<ul id="<?php _e( $atts['id'] );?>" data-target="<?php _e('li.post-item');?>" data-url="<?php _e( $atts['url'] );?>" class="list-unstyled shift-grid-3">
+  <?php while( $this->query->have_posts() ) : $this->query->the_post();?>
+    <li class="post-item">
+      <?php get_template_part( 'partials/content', 'grid'); ?>
+    </li>
+  <?php endwhile;?>
+</ul>
---
+++ partials/content-grid.php
@@ -0,0 +1,9 @@
+<div class="post-wrapper">
+  <div class="post-inner">
+    <h2 class="title"><?php the_title();?></h2>
+    <div class="content">
+      <?php the_content();?>
+    </div>
+    <a class="post-btn" href="#"role="button">Get help for your teen now</a>
+  </div>
+</div>
