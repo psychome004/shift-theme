e6f1d3c4f64f7672bc59ca6651dd9b210886b51b
psychome004 <anil.stephen66@gmail.com>
2021-03-30 12:47:28 +0530

    Updated orbit-3-grid template

--- partials/content-grid.php
+++ partials/content-grid.php
@@ -2,8 +2,8 @@
   <div class="post-inner">
     <h2 class="title"><?php the_title();?></h2>
     <div class="content">
-      <?php the_content();?>
+      <?php the_excerpt();?>
     </div>
-    <a class="post-btn" href="#"role="button">Get help for your teen now</a>
+    <a class="post-btn" href="<?php the_permalink(); ?>" role="button">Read More</a>
   </div>
 </div>
