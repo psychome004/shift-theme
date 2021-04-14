25c372e67d0c7d111d2404b6a4091acb6c7ab671
psychome004 <anil.stephen66@gmail.com>
2021-04-14 14:36:59 +0530

    Updated orbit-grid-4 template

--- orbit-query/articles-grid-4.php
+++ orbit-query/articles-grid-4.php
@@ -1,4 +1,4 @@
-<ul id="<?php _e( $atts['id'] );?>" data-target="<?php _e('li.post-item');?>" data-url="<?php _e( $atts['url'] );?>" class="list-unstyled shift-grid-3">
+<ul id="<?php _e( $atts['id'] );?>" data-target="<?php _e('li.post-item');?>" data-url="<?php _e( $atts['url'] );?>" class="list-unstyled shift-grid-4">
   <?php while( $this->query->have_posts() ) : $this->query->the_post();?>
     <li class="post-item">
       <?php get_template_part( 'partials/content', 'grid'); ?>
--- partials/content-grid.php
+++ partials/content-grid.php
@@ -1,9 +1,13 @@
-<div class="post-wrapper">
-  <div class="post-inner">
+<div class="post-card">
+  <div class="front">
     <h2 class="title"><?php the_title();?></h2>
-    <div class="content">
-      <?php the_excerpt();?>
-    </div>
-    <a class="post-btn" href="<?php the_permalink(); ?>" role="button">Read More</a>
   </div>
+  <a class="post-url" href="<?php the_permalink(); ?>" role="button">
+    <div class="back">
+      <div class="content">
+        <?php the_excerpt();?>
+      </div>
+      <span class="btn">Read More</span>
+    </div>
+  </a>
 </div>
