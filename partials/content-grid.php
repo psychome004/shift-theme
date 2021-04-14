3a5811311c250236dd72e666243f9d48edfaff32
psychome004 <anil.stephen66@gmail.com>
2021-04-14 15:25:09 +0530

    Updated orbit-grid-4 template

--- partials/content-grid.php
+++ partials/content-grid.php
@@ -1,9 +1,11 @@
+<?php $image_url = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'full' ); ?>
 <div class="post-card">
-  <div class="front">
+  <div class="front" style="background-image: url(<?php echo( $image_url ? $image_url[0] : '' );?>)">
     <h2 class="title"><?php the_title();?></h2>
   </div>
   <a class="post-url" href="<?php the_permalink(); ?>" role="button">
     <div class="back">
+      <h4 class="title"><?php the_title();?></h4>
       <div class="content">
         <?php the_excerpt();?>
       </div>
