e9f4bb1ee7c7e6b74ca3305d4eeac12dcddb714b
psychome004 <anil.stephen66@gmail.com>
2021-04-21 17:35:44 +0530

    Updated shift-simple-card

--- so-widgets/shift-simple-card/templates/template.php
+++ so-widgets/shift-simple-card/templates/template.php
@@ -1,7 +1,7 @@
 <!-- SHIFT SIMPLE CARD -->
 <div class="card-container" style="max-width: 1170px;">
-<?php foreach ( $instance['simple_cards_repeater'] as $simple_card ) :?>
-  <div class="card-simple-inner" style="background-color:<?php _e( $instance['bg_color'] );?>;">
+<?php foreach ( $instance['simple_cards_repeater'] as $key => $simple_card ) :?>
+  <div class="card-simple-inner" style="background-color:<?php _e( $instance['bg_color'] );?>;" data-aos="fade-up" data-aos-delay="<?php _e( $key * 200 );?>">
     <div class="simple-card-body">
       <div class="simple-card-header" style="color:<?php _e( $instance['title_color'] );?>">
         <h4><?php _e( $simple_card['card_title'] );?></h4>
